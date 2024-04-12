2fd43a27e601af2bdf9b96656f49db44c2daeecb
Mund3k <kubat1611@gmail.com>
2024-04-12 18:12:09 +0700

    Pydantic changes

--- src/airQuality.py
+++ src/airQuality.py
@@ -1,54 +1,80 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from typing import Optional
 
 
 class AirQuality(BaseModel):
-    timestamp: Optional[str]
-    aqi_US: Optional[int]
-    aqi_China: Optional[int]
-    main_pollutant_US: Optional[str]
-    main_pollutant_China: Optional[str]
-    temperature: Optional[int]
-    atmospheric_pressure: Optional[int]
-    humidity: Optional[int]
-    wind_speed: Optional[float]
-    wind_direction: Optional[int]
+    _timestamp: Optional[str]
+    _aqi_US: Optional[int]
+    _aqi_China: Optional[int]
+    _main_pollutant_US: Optional[str]
+    _main_pollutant_China: Optional[str]
+    _temperature: Optional[int]
+    _atmospheric_pressure: Optional[int]
+    _humidity: Optional[int]
+    _wind_speed: Optional[float]
+    _wind_direction: Optional[int]
+
+    @classmethod
+    @field_validator("_timestamp")
+    def validate_timestamp(cls, value):
+        return value
+
+    @classmethod
+    @field_validator("_aqi_US")
+    def validate_aqi_us(cls, value):
+        if value is not None and (value < 0 or value > 500):
+            raise ValueError("AQI_US must be between 0 and 500")
+        return value
+
+    @classmethod
+    @field_validator("_aqi_China")
+    def validate_aqi_china(cls, value):
+        if value is not None and (value < 0 or value > 500):
+            raise ValueError("AQI_China must be between 0 and 500")
+        return value
+
+    @classmethod
+    @field_validator("_main_pollutant_US", "_main_pollutant_China")
+    def validate_pollutants(cls, values):
+        return values
+
+    @classmethod
+    @field_validator("_temperature", "_atmospheric_pressure", "_humidity")
+    def validate_weather_values(cls, values):
+        for value in values:
+            if value is not None and value < 0:
+                raise ValueError("Temperature, pressure, and humidity cannot be negative")
+        return values
+
+    @classmethod
+    @field_validator("_wind_speed")
+    def validate_wind_speed(cls, value):
+        if value is not None and value < 0:
+            raise ValueError("Wind speed cannot be negative")
+        return value
 
     def add_data(self, data: dict[str, Optional[dict[str, Optional[float]]]]):
         for key in ['pollution', 'weather']:
             if key not in data:
                 data[key] = None
 
-        self.timestamp = data['pollution'].get('ts')
-        self.aqi_US = data['pollution'].get('aqius')
-        self.aqi_China = data['pollution'].get('aqicn')
-        self.main_pollutant_US = data['pollution'].get('mainus')
-        self.main_pollutant_China = data['pollution'].get('maincn')
-        self.temperature = data['weather'].get('tp')
-        self.atmospheric_pressure = data['weather'].get('pr')
-        self.humidity = data['weather'].get('hu')
-        self.wind_speed = data['weather'].get('ws')
-        self.wind_direction = data['weather'].get('wd')
+        pollution_data = data.get('pollution', {})
+        weather_data = data.get('weather', {})
 
-        if self.temperature >= 100 or self.temperature <= -100:
-            raise ValueError('Invalid temperature')
-        if self.atmospheric_pressure >= 2000 or self.atmospheric_pressure <= 500:
-            raise ValueError('Invalid athmospheric pressure')
-
+        self._timestamp = pollution_data.get('ts')
+        self._aqi_US = pollution_data.get('aqius')
+        self._aqi_China = pollution_data.get('aqicn')
+        self._main_pollutant_US = pollution_data.get('mainus')
+        self._main_pollutant_China = pollution_data.get('maincn')
+        self._temperature = weather_data.get('tp')
+        self._atmospheric_pressure = weather_data.get('pr')
+        self._humidity = weather_data.get('hu')
+        self._wind_speed = weather_data.get('ws')
+        self._wind_direction = weather_data.get('wd')
 
     def get_data(self) -> tuple[str, int, int, str, str, int, int, int, float, int]:
         data = (
-            self.timestamp, self.aqi_US, self.aqi_China, self.main_pollutant_US, self.main_pollutant_China,
-            self.temperature, self.atmospheric_pressure, self.humidity, self.wind_speed, self.wind_direction
+            self._timestamp, self._aqi_US, self._aqi_China, self._main_pollutant_US, self._main_pollutant_China,
+            self._temperature, self._atmospheric_pressure, self._humidity, self._wind_speed, self._wind_direction
         )
-        self.timestamp = None
-        self.aqi_US = None
-        self.aqi_China = None
-        self.main_pollutant_US = None
-        self.main_pollutant_China = None
-        self.temperature = None
-        self.atmospheric_pressure = None
-        self.humidity = None
-        self.wind_speed = None
-        self.wind_direction = None
         return data
